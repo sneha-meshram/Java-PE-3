94be836a1d37e18cf92d7962e5dfab85c9fddab6
sneha <sneha.meshram@cgi.com>
2019-07-25 22:19:00 +0530

    Add Exception Class

---
+++ src/main/java/com/stackroute/pe3/ExceptionGenerate.java
@@ -0,0 +1,30 @@
+package com.stackroute.pe3;
+
+public class ExceptionGenerate {
+    public static void main(String[] args) {
+
+
+       // IndexOutOfBoundsException
+        try{
+            int arr[]=new int[3];
+            arr[3]=30;
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("IndexOutOfBoundsException");
+        }
+
+        //NegativeArraySizeException
+        try{
+            int arr[]=new int[-3];
+        }catch(NegativeArraySizeException e){
+            System.out.println("NegativeArraySizeException");
+        }
+        // NullPointerException
+        try{
+            int arr[]=null;
+            int lengthOfarray=arr.length;
+        }catch(NullPointerException e){
+            System.out.println("NullPointerException");
+        }
+    }
+
+}
---
+++ src/test/java/com/stackroute/pe3/ExceptionGenerateTest.java
@@ -0,0 +1,39 @@
+package com.stackroute.pe3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExceptionGenerateTest {
+
+    ExceptionGenerate Exception;
+    @Before
+    public void setUp(){
+        Exception = new ExceptionGenerate();
+    }
+
+    @After
+    public void tearDown(){
+        Exception=null;
+    }
+
+    @Test(expected = NegativeArraySizeException.class)
+    public void givenArrayExceptionShouldThrowNegativeArraySizeExcpetion() {
+        Exception.givenException("value");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void givenArraySizeIsOutOfBound() {
+        System.out.println("Inside testPrintMessage()");
+        Exception.givenException(String[] array);
+    }
+
+    @Test(expected = NegativeArraySizeException.class)
+    public void givenArrayExceptionShouldThrowNullPointerExcpetion() {
+        Exception.givenException(array);
+
+    }
+
+}
